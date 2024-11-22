50d20c62a6b563076c23f172223c3c97056182e7
MamaFissy <findlayast@gmail.com>
2024-11-22 14:24:29 -0400

    created GameManager script
    demo is in tower-shooting-test. modified waveCreator and enemyHealth to allow for functionality
    
    Co-Authored-By: Lore1044 <185219869+Lore1044@users.noreply.github.com>

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] waves;
+    public GameObject buildPhaseUI;
+
+    private bool inWave = false;
+    private int waveCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildPhaseUI.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when press enter transition to next wave
+        if (Input.GetKeyDown(KeyCode.Return) && !inWave)
+        {
+            buildPhaseUI.SetActive(false);
+            spawnWave();
+        }
+
+        if (!inWave)
+        {
+            buildPhase();
+        }
+    }
+
+    void buildPhase()
+    {
+        //pop up canvas
+        buildPhaseUI.SetActive(true);
+    }
+    //spawns the game object to start specified wave based on waveCounter
+    void spawnWave()
+    {
+        Instantiate(waves[waveCounter], transform.position, Quaternion.identity);
+        inWave = true;
+        Debug.Log("InWave set to true");
+        waveCounter++;
+    }
+
+    public void waveOver()
+    {
+        inWave = false;
+        Debug.Log("InWave set to false");
+    }
+}
--- Assets/Scripts/waveCreator.cs
+++ Assets/Scripts/waveCreator.cs
@@ -10,10 +10,17 @@ public class waveCreator : MonoBehaviour
 
     private int offset;
     private int enemyCounter = 0;
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+    private GameManager gameManager;
 
     void Start()
     {
         offset = spawnOffset;
+        gameManager = FindObjectOfType<GameManager>();
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager not found in the scene!");
+        }
     }
 
     void Update()
@@ -24,11 +31,30 @@ public class waveCreator : MonoBehaviour
             offset = spawnOffset;
             generateEnemy();
         }
+
+        // Check if all spawned enemies are destroyed
+        if (enemyCounter == (enemies.Length) && AllEnemiesDestroyed())
+        {
+            gameManager.waveOver();
+            Destroy(gameObject); // Optionally destroy this wave object when done
+        }
     }
 
     private void generateEnemy()
     {
-        Instantiate(enemies[enemyCounter], spawnPoint, Quaternion.identity);
+        GameObject enemy = Instantiate(enemies[enemyCounter], spawnPoint, Quaternion.identity);
+        spawnedEnemies.Add(enemy);
         enemyCounter++;
+    }
+
+    private bool AllEnemiesDestroyed()
+    {
+        // Remove any null references from the list (destroyed enemies)
+        spawnedEnemies.RemoveAll(enemy => enemy == null);
+        // If the list is empty, all enemies are destroyed
+        Debug.Log(spawnedEnemies.Count);
+        return spawnedEnemies.Count == 0;
+
+       
     }
 }
